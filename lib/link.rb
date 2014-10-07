2219f82c6ba68a3135db898bdb669beeb82ab4d9
galicians <galicians@gmail.com>
2014-10-07 14:34:31 +0100

    user & password validation added

--- lib/link.rb
+++ lib/link.rb
@@ -5,11 +5,12 @@ class Link
   # this makes the instances of this class Datamapper resources
   include DataMapper::Resource
 
-  has n, :tags, :through => Resource
+    has n, :tags, :through => Resource
 
   # This block describes what resources our model will have
   property :id,     Serial # Serial means that it will be auto-incremented for every record
   property :title,  String
   property :url,    String
+  
 
 end
--- lib/server.rb
+++ lib/server.rb
@@ -48,6 +48,8 @@ get '/users/new' do
 end
 
 post '/users' do
+
+	puts ""
 	@user = User.create(:email => params[:email],
 							:password => params[:password],
 							:password_confirmation => params[:password_confirmation])
@@ -55,7 +57,7 @@ post '/users' do
 		session[:user_id] = @user.id
 		redirect to('/')
 	else
-		flash[:notice] = "Sorry, your passwords don't match"
+		flash.now[:errors] = @user.errors.full_messages
 		erb :"users/new"
 	end
 end
