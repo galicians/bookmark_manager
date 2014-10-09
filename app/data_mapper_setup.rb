b48cc8ccb564f740f3fa6eaf96dd81fdec535270
galicians <galicians@gmail.com>
2014-10-09 10:33:56 +0100

    Updated data_mapper_setup file for heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,6 +4,5 @@ require_relative 'models/link'
 require_relative 'models/tag'
 require_relative 'models/user'
 
-DataMapper.setup(:default, ENV["DATABASE_URL"] || postgres://localhost/bookmark_manager_#{env}")
-
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 DataMapper.finalize
